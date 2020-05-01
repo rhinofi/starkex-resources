a175b1b7fc240c6a6394a1c2fd1e00be74f3f050
Adrian Gierakowski <agierakowski@gmail.com>
2020-05-01 11:19:24 +0100

    committee: implement is_valid
    post vaults and batchId to url defined in BATCH_VALIDATION_URL env var

--- committee/committee/custom_validation.py
+++ committee/committee/custom_validation.py
@@ -1,3 +1,7 @@
+import io
+
+import requests
+
 from starkware.objects.availability import StateUpdate
 
 
@@ -5,4 +9,16 @@ async def is_valid(state_update: StateUpdate, batch_id: int) -> bool:
     """
     A hook for third parties to validate the state_update before signing the new root.
     """
-    return True
+
+    # Using io.environ instead of getenv to throw an error if the env var is not set
+    url = io.environ['BATCH_VALIDATION_URL']
+
+    # We only need vaults which contain balances for all vaults which are
+    # affected by the batch.
+    payload = { 'vaults': state_update.vaults, 'batchId': batch_id }
+    response = requests.post(url, data = payload)
+
+    if response.status_code == 200 or response.status_code == 204:
+      return True
+
+    return False
